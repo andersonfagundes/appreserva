823b83f8362a40be19e8fdc631e4a29d3b1609b5
unknown <anderson.fagundes4@gmail.com>
2021-12-04 00:08:28 -0300

    Criado atributos e metodos get e set para a classe Alimentacao

--- src/main/java/br/edu/infnet/appreserva/model/domain/Alimentacao.java
+++ src/main/java/br/edu/infnet/appreserva/model/domain/Alimentacao.java
@@ -2,4 +2,28 @@ package br.edu.infnet.appreserva.model.domain;
 
 public class Alimentacao extends Servico {
 	
+	private Integer id;
+	private boolean cafeDaManha;
+	private boolean almoco;
+	private boolean Jantar;
+	
+	public boolean isCafeDaManha() {
+		return cafeDaManha;
+	}
+	public void setCafeDaManha(boolean cafeDaManha) {
+		this.cafeDaManha = cafeDaManha;
+	}
+	public boolean isAlmoco() {
+		return almoco;
+	}
+	public void setAlmoco(boolean almoco) {
+		this.almoco = almoco;
+	}
+	public boolean isJantar() {
+		return Jantar;
+	}
+	public void setJantar(boolean jantar) {
+		Jantar = jantar;
+	}
+	
 }
