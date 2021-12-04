bc2f108770acb7233ad481c9f5d01a133d91eed2
unknown <anderson.fagundes4@gmail.com>
2021-12-04 00:16:42 -0300

    Criado atributos e metodos get e set para a classe Lazer

--- src/main/java/br/edu/infnet/appreserva/model/domain/Lazer.java
+++ src/main/java/br/edu/infnet/appreserva/model/domain/Lazer.java
@@ -1,5 +1,29 @@
 package br.edu.infnet.appreserva.model.domain;
 
 public class Lazer extends Servico {
- 
+	
+	private Integer id;
+	private boolean aluguelChurrasqueira;
+	private boolean servicoSpa;
+	private boolean servicoMassagem;
+	
+	public boolean isAluguelChurrasqueira() {
+		return aluguelChurrasqueira;
+	}
+	public void setAluguelChurrasqueira(boolean aluguelChurrasqueira) {
+		this.aluguelChurrasqueira = aluguelChurrasqueira;
+	}
+	public boolean isServicoSpa() {
+		return servicoSpa;
+	}
+	public void setServicoSpa(boolean servicoSpa) {
+		this.servicoSpa = servicoSpa;
+	}
+	public boolean isServicoMassagem() {
+		return servicoMassagem;
+	}
+	public void setServicoMassagem(boolean servicoMassagem) {
+		this.servicoMassagem = servicoMassagem;
+	}
+	
 }
