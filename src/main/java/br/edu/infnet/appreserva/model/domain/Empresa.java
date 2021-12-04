d084f8c43c602adb5f099617f1695e38e8104769
unknown <anderson.fagundes4@gmail.com>
2021-12-03 23:06:47 -0300

    Implementado o toString na classe Empresa usando o StringBuilder

--- src/main/java/br/edu/infnet/appreserva/model/domain/Empresa.java
+++ src/main/java/br/edu/infnet/appreserva/model/domain/Empresa.java
@@ -7,6 +7,14 @@ public class Empresa {
 	private String cnpj;
 	private String telefone;
 	
+	public Empresa(String nome, String cnpj, String telefone) {
+		this.nome = nome;
+		this.cnpj = cnpj;
+		this.telefone = telefone;
+	}
+	
+	StringBuilder dados = new StringBuilder();
+	
 	public String getNome() {
 		return nome;
 	}
@@ -26,10 +34,13 @@ public class Empresa {
 		this.telefone = telefone;
 	}
 	
-	public Empresa(String nome, String cnpj, String telefone) {
-		this.nome = nome;
-		this.cnpj = cnpj;
-		this.telefone = telefone;
+	@Override
+	public String toString() {
+		//return "Nome: "+this.nome+";"+"Cnpj: "+this.cnpj+";"+"Telefone: "+this.telefone;
+		 dados.append("Nome: ").append(nome).append(";").append("Cnpj: ").append(cnpj)
+				.append(";").append("Telefone: ").append(telefone);
+		
+		 return dados.toString();		
 	}
 	
 }
