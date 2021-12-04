4f03d691112b41ea967e4a22ccab47aaabeeb38e
unknown <anderson.fagundes4@gmail.com>
2021-12-03 22:44:40 -0300

    Criado os métodos get e set para os atributos da classe Empresa

--- src/main/java/br/edu/infnet/appreserva/model/domain/Empresa.java
+++ src/main/java/br/edu/infnet/appreserva/model/domain/Empresa.java
@@ -1,8 +1,29 @@
 package br.edu.infnet.appreserva.model.domain;
 
 public class Empresa {
+	
 	private Integer id;
 	private String nome;
 	private String cnpj;
 	private String telefone;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
 }
