aacdb632c8a825afee8dba922b5f783165d270d4
Anderson Fagundes <anderson.fagundes4@gmail.com>
2021-12-11 15:38:02 -0300

    Adicionado EmpresaService

---
+++ src/main/java/br/edu/infnet/appreserva/model/service/EmpresaService.java
@@ -0,0 +1,41 @@
+package br.edu.infnet.MyAppPedido.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.infnet.MyAppPedido.model.domain.Usuario;
+import br.edu.infnet.appreserva.model.domain.Empresa;
+
+@Service
+public class EmpresaService {
+	
+	@Autowired
+	private EmpresaService empresaRepository;
+
+	public List<Empresa> obterLista(){
+
+		return (List<Empresa>) empresaRepository.findAll();
+	}
+	
+	public List<Empresa> obterLista(Usuario usuario){
+
+		return (List<Empresa>) empresaRepository.findAll(usuario.getId());
+	}
+
+	public void incluir(Empresa solicitante) {
+
+		empresaRepository.save(solicitante);
+	}
+	
+	public void excluir(Integer id) {
+
+		empresaRepository.deleteById(id);
+	}
+	
+	public Empresa obterPorId(Integer id) {
+
+		return empresaRepository.findById(id).orElse(null);
+	}
+}
