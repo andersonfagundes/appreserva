0f08f1e00be2bc16e10175d1e11958dcf9d60a41
Anderson Fagundes <anderson.fagundes4@gmail.com>
2021-12-11 15:30:18 -0300

    Adcionado EmpresaRepository

---
+++ src/main/java/br/edu/infnet/appreserva/model/repository/EmpresaRepository.java
@@ -0,0 +1,16 @@
+package br.edu.infnet.appreserva.model.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.infnet.appreserva.model.domain.Empresa;
+
+@Repository
+public interface EmpresaRepository extends CrudRepository<Empresa, Integer> {
+
+	@Query("from Solicitante s where s.usuario.id = :userid")
+	public List<Empresa> findAll(Integer userid);
+}
