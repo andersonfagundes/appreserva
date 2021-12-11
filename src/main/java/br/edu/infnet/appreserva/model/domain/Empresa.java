2fe35d032063f202805688458ac2ee5301eacb6a
Anderson Fagundes <anderson.fagundes4@gmail.com>
2021-12-11 15:48:14 -0300

    Alterado Empresa domain

--- src/main/java/br/edu/infnet/appreserva/model/domain/Empresa.java
+++ src/main/java/br/edu/infnet/appreserva/model/domain/Empresa.java
@@ -1,11 +1,30 @@
 package br.edu.infnet.appreserva.model.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TEmpresa")
 public class Empresa {
 	
-	private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;	
 	private String nome;
 	private String cnpj;
 	private String telefone;
+	@ManyToOne
+	@JoinColumn(name = "idUsuario")
+	private Usuario usuario;
+	
+	public Empresa() {
+		//
+	}
 	
 	public Empresa(String nome, String cnpj, String telefone) {
 		this.nome = nome;
@@ -13,34 +32,57 @@ public class Empresa {
 		this.telefone = telefone;
 	}
 	
-	StringBuilder dados = new StringBuilder();
-	
+	@Override
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(id);
+		sb.append(";");
+		sb.append(nome);
+		sb.append(";");
+		sb.append(cnpj);
+		sb.append(";");
+		sb.append(telefone);
+
+		return sb.toString();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
 	public String getCnpj() {
 		return cnpj;
 	}
+
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	
-	@Override
-	public String toString() {
-		//return "Nome: "+this.nome+";"+"Cnpj: "+this.cnpj+";"+"Telefone: "+this.telefone;
-		 dados.append("Nome: ").append(nome).append(";").append("Cnpj: ").append(cnpj)
-				.append(";").append("Telefone: ").append(telefone);
-		
-		 return dados.toString();		
-	}
-	
 }
